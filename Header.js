a767e77772d2b18992accc5c0c3f74967c3c49f7
Jatin Bansal <48070356+Jatin110@users.noreply.github.com>
2020-10-24 21:23:22 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+import { Link} from "react-router-dom"
+function  Header() {
+    return (
+        <nav className="header_logo">
+        {/* <img src="logo.png" />  */}
+        </nav> 
+    )
+}
+
+export default Header;
